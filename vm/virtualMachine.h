4ed00b837874daa6edc21e3c1b6ecde030735ba3
LittleTheFu <lttlstpd@live.com>
2025-07-18 18:43:00 +0800

    add virtual machine(is not finished yet)

---
+++ vm/virtualMachine.h
@@ -0,0 +1,11 @@
+#ifndef _VIRTUAL_MACHINE_H_
+#define _VIRTUAL_MACHINE_H_
+
+class VirtualMachine
+{
+public:
+    VirtualMachine();
+    ~VirtualMachine() = default;
+};
+
+#endif // _VIRTUAL_MACHINE_H_
