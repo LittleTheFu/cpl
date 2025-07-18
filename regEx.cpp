e07650cf703942a1a493514266853d3879728c60
LittleTheFu <lttlstpd@live.com>
2025-07-18 10:13:14 +0800

    update regEx

--- regEx.cpp
+++ regEx.cpp
@@ -1,1 +1,15 @@
 #include "regEx.h"
+
+// RegEx::RegEx(const std::string &regex)
+// {
+//     regex_ = regex;
+// }
+
+RegEx::RegEx(std::shared_ptr<RegExNode> root)
+{
+    root_ = root;
+}
+
+RegEx::~RegEx()
+{
+}
--- regEx.h
+++ regEx.h
@@ -1,8 +1,20 @@
 #ifndef _REGEX_H_
 #define _REGEX_H_
 
+#include <memory>
+#include <string>
+#include "regExNode.h"
+
 class RegEx
 {
+public:
+    // explicit RegEx(const std::string& regex);
+    explicit RegEx(std::shared_ptr<RegExNode> root);
+    ~RegEx();
+
+private:
+    // std::string regex_;
+    std::shared_ptr<RegExNode> root_;
 
 };
 
