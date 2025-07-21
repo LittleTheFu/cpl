32eceba8225e53b4a88dacb03e10f05fee8b7f69
LittleTheFu <lttlstpd@live.com>
2025-07-21 12:19:16 +0800

    save

--- regluarExpression/regExParser.cpp
+++ regluarExpression/regExParser.cpp
@@ -1,1 +1,55 @@
 #include "regExParser.h"
+
+RegExParser::RegExParser(const std::string &regExStr)
+{
+    regExStr_ = regExStr;
+    index_ = 0;
+}
+
+void RegExParser::parseRegEx()
+{
+    // std::pair<bool, char> ch = nextChar();
+    parseTerm();
+
+    std::pair<bool, char> c = nextChar();
+
+    if(c.first && c.second == '|')
+    {
+        parseChar();
+    }
+}
+
+void RegExParser::parseTerm()
+{
+    parseFactor();
+}
+
+void RegExParser::parseFactor()
+{
+    parseAtom();
+}
+
+void RegExParser::parseAtom()
+{
+    parseChar();
+}
+
+void RegExParser::parseChar()
+{
+    if (index_ >= regExStr_.size())
+    {
+        return;
+    }
+}
+
+std::pair<bool, char> RegExParser::nextChar()
+{
+    if (index_ >= regExStr_.size())
+    {
+        return {false, '\0'};
+    }
+    
+    index_++;
+
+    return {true, regExStr_[index_ - 1]};
+}
--- regluarExpression/regExParser.h
+++ regluarExpression/regExParser.h
@@ -1,5 +1,34 @@
 #ifndef _REG_EX_PARSER_H_
 #define _REG_EX_PARSER_H_
 
+#include <string>
+
+// regex       ::= term { '|' term }
+// term        ::= factor { factor }
+// factor      ::= atom [ '*' | '+' | '?' ]
+// atom        ::= char | '(' regex ')'
+// char        ::= letter | digit | '_' | ...
+
+class RegExParser
+{
+public:
+    RegExParser(const std::string &regExStr);
+    ~RegExParser() = default;
+
+private:
+    void parseRegEx();
+    void parseTerm();
+    void parseFactor();
+    void parseAtom();
+    void parseChar();
+
+    std::pair<bool, char> nextChar();
+
+private:
+    std::string regExStr_;
+    std::size_t index_;
+
+}
+
 
 #endif // _REG_EX_PARSER_H_
